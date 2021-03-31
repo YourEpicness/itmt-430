216f219c2989c57b6cf895a85e987f3214bfbcb9
Julaam Diop <jeebe.diop@gmail.com>
2021-03-31 00:20:14 -0500

    sprint2 db

--- 2021-team3-sample/sprint-02/code/db-samples/insert-records.sql
+++ 2021-team3-sample/sprint-02/code/db-samples/insert-records.sql
@@ -1,4 +1,4 @@
 -- Create a small script to enter 3 real pieces of data
 USE posts;
 
-INSERT INTO comment (id, Title, PosterName, Content) Values (0,"New Idea","Abraham Lincoln","Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal."),(0,"Someone is wrong on the internet","Annie Mouse","https://www.xkcd.com/386/"),(0,"Presidential Quote","President James Madison","If men were angels, no government would be necessary");
+INSERT INTO users (user_id, first_name, last_name, email, phone_number, student_id, request_item, karma_points) Values (1, "John", "Doe", "johndoe@gmail.com", 1234567890, 1234567, "y", 100);
